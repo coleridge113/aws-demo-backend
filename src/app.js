e1890d473846f4bb7c0a71d39c19024b5f10474b
Jose Luna <josemanuelmluna@gmail.com>
2026-06-25 17:58:20 +0800

    feat: added endpoint for lambda

--- src/app.js
+++ src/app.js
@@ -30,4 +30,9 @@ app.post('/login', (req, res) => {
     res.json({ token });
 });
 
+app.post('/webhook', (req, res) => {
+  console.log("Received event:", req.body);
+  res.status(200).send('Webhook received');
+});
+
 app.listen(port, () => console.log('Server running on port', port));
